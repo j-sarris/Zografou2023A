b9aaee351023a9b4df9ee77377a582f2b2f2850b
Yannis <79635419+jsarris75@users.noreply.github.com>
2023-02-28 12:29:53 +0200

    Create example_age.py

---
+++ Python/example_age.py
@@ -0,0 +1,8 @@
+# Ask birth year, calculate age and print it on the terminal
+
+birth_year = input('Give your birth year: ')
+age = 2023 - int(birth_year)
+print(f'You are {age} years old.')
+
+print(type(birth_year))
+print(type(age))
