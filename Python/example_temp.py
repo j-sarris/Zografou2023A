17edb88c6d285a7bf5926d23cd52f6222dd8e22f
Yannis <79635419+jsarris75@users.noreply.github.com>
2023-03-09 08:55:55 +0200

    Update example_temp.py

--- Python/example_temp.py
+++ Python/example_temp.py
@@ -1,11 +1,20 @@
-# convert Celsius degrees to Fahrenheit
+ch = input("(C)elsius to Fahreneit or (F)ahreneit to Celsius?: ") 
 
-temp_cels = input("Give temperature in Celsius degrees: ")
-temp_fahr = int(temp_cels) * 1.8 + 32
+if ch.lower() == "c":
 
-print(f"Temperature in Fahreneit degrees is: {temp_fahr}")
+    # convert Celsius degrees to Fahrenheit
 
+    temp_cels = input("Give temperature in Celsius degrees: ")
+    temp_fahr = float(temp_cels) * 1.8 + 32
 
-# Modify the file so as to also convert the other way around 
+    print(f"Temperature in Fahreneit degrees is: {temp_fahr}")
+
+elif ch.lower() == "f":
+    temp_fahr = input("Give temperature in Fahreneit degrees: ")
+    temp_cels = (float(temp_fahr) - 32) * 5/9
+
+    print(f"Temperature in Celsius degrees is: {temp_cels}")
+
+# Modify as to convert the other way around 
 # Degrees Fahrenheit to Celsius, subtract 32 and multiply by 5/9
-# Ask the user to decide which conversion to perform
+# Ask the user for which conversion
