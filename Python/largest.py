0137f42b2b269898bf4b174894d54c6d3aeb5214
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-06-06 09:32:29 +0300

    Add files via upload

---
+++ Python/largest.py
@@ -0,0 +1,10 @@
+list = [4, 5, 2, 8, 1]
+
+max = list[0]
+for item in list:
+    if item > max:
+        max=item
+
+print(f"Maximum item is {max}")
+
+# COME BACK LATER AND ORGANISE AS FUNCTION IN DIFFERENT MODULES
